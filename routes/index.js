5c138acebaa0fe63857c004c641a6411e1d6167e
Abhiram4444 <padarajuabhiram@gmail.com>
2024-08-05 14:30:48 +0530

    second commit

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'keerthi loves abhirama rao' });
+  res.render('index', { title: 'keerthi loves abhiram' });
 });
 
 module.exports = router;
